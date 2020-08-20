03166fb99d297ab8274d66fb3a89b4e0171199c4
Your Name <your@email.com>
2020-08-20 18:02:53 +0200

    initial commit

---
+++ src/test/java/com/dipnoi/CSVreader/CsVreaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dipnoi.CSVreader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsVreaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
